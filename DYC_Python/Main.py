8027d4ac2911c7a4a2b3a0d626824999734dc2c7
paullee100 <leepaul863@gmail.com>
2023-05-08 13:09:10 -0700

    Updated Python Version and included captures

--- DYC_Python/Main.py
+++ DYC_Python/Main.py
@@ -6,24 +6,23 @@ class Main:
         pass
     
     def main(self):
-        # os.chdir(os.path.dirname(__file__))
-        # os.chdir("/..")
         os.chdir('TCSS483_DefendYourCode')
-        # print(os.getcwd()) 
-        errorsLog = open('./errorlog.txt', 'a')
-        passwordStorage = open('./password.txt', 'w+b')
+        errorsLog = open('errorlog.txt', 'a')
+        passwordStorage = open('password.txt', 'w+b')
         
         # Enter a first name
-        firstName = self.__name(errorsLog)
+        firstName = self.__name(errorsLog, "first")
         # Enter a last name
-        lastName = self.__name(errorsLog)
+        lastName = self.__name(errorsLog, "last")
         
         # Sum and Multiply two integers
         sum, prod, firstDigit, secondDigit = self.__sum(errorsLog)
         
         # Function for input/output files
-        inputFile = self.__readInputFile(errorsLog)
-        outputFile = self.__readInputFile(errorsLog, inputFile)
+        print("Now reading Input file.")
+        inputFile = self.__readInputFile(errorsLog, "input")
+        print("Now reading Output file.")
+        outputFile = self.__readInputFile(errorsLog, "output", inputFile)
         
         # Enter password
         # Salt and Hash Password
@@ -36,11 +35,11 @@ class Main:
         errorsLog.close()
         passwordStorage.close()
     
-    def __name(self, errorsLog):
+    def __name(self, errorsLog, section):
         nameString = ""
         matches = False
         while not matches:
-            nameString = input("Enter a valid name (Start with a capital letter followed by a-z or a '-'): ")
+            nameString = input(f"\nEnter a valid {section} name (Start with a capital letter followed by a-z or a '-'): ")
             matches = re.match("^[A-Z][-a-z]{1,49}$", nameString)
             if not matches:
                 errorsLog.write("ERROR in name(): Invalid Name Entered\n")
@@ -53,7 +52,7 @@ class Main:
         flag = False
         while not flag:
             try:
-                i1 = int(input("Enter a valid int (Between -2147483648 and 2147483647): "))
+                i1 = int(input("\nEnter a valid int (Between -2147483648 and 2147483647): "))
                 i2 = int(input("Enter another valid int (Between -2147483648 and 2147483647): "))
                 
                 if i1 > 2147483647 or i2 > 2147483647:
@@ -80,6 +79,7 @@ class Main:
                     prod = i1 * i2
             except:
                 errorsLog.write("ERROR in sum(): Value entered is not a int\n")
+                print("Input must be an integer")
         
         return sum, prod, i1, i2
     
@@ -87,14 +87,14 @@ class Main:
         passwordString = ""
         matches = False
         while not matches:
-            print("Password must be at least 10 characters long and contains:")
+            print("\nPassword must be at least 10 characters long and contains:")
             print("at least 1 uppercase character, 1 lowercase character, 1 digit character")
             print("and 1 punctuation. No whitespaces allowed")
             passwordString = input("Enter a valid password: ")
-            # if passwordString.__contains__(' '):
-            #     errorsLog.write("ERROR in createPassword(): Password contains a space\n")
-            #     print("Password cannot contain a space")
-            #     continue
+            if passwordString.__contains__(' '):
+                errorsLog.write("ERROR in createPassword(): Password contains a space\n")
+                print("Password cannot contain a space")
+                continue
             
             matches = re.match("(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?]).{10,}", passwordString)
             if not matches:
@@ -113,14 +113,14 @@ class Main:
         matches = False
         
         while not matches:
-            passwordString = input("Verify your password: ")
+            passwordString = input("\nVerify your password: ")
             
             try:
                 hash.seek(0)
                 currPass = hash.readlines()[0]
             except:
                 print("File Not Found")
-                errorsLog.write("ERROR in verifyPassword(): File Not Found")
+                errorsLog.write("ERROR in verifyPassword(): File Not Found\n")
                 return
 
             passwordString = passwordString.encode('utf-8')
@@ -131,27 +131,27 @@ class Main:
                 print("Password matches")
             else:
                 print("Password does not match")
-                errorsLog.write("ERROR in verifyPassword(): Passwords do not match.")
+                errorsLog.write("ERROR in verifyPassword(): Passwords do not match.\n")
                 
-    def __readInputFile(self, errorsLog, *string):
+    def __readInputFile(self, errorsLog, section, *string):
         fileName = ""
         valid = False
         while not valid:
-            print('Text file must end in .txt and cannot have the following characters:')
+            print('\nText file must end in .txt and cannot have the following characters:')
             print('<, >, :, \", /, \\, |, ?, *')
-            fileName = input("Please type in a valid text file name: ")
+            fileName = input(f"Please type in a valid {section} text file name: ")
             valid = re.match("^(?!password\.txt$|errorlog\.txt$)[a-zA-Z0-9!@#$%^&()_+=-]+\.txt$", fileName)
 
             for i in string:
                 if i != None and fileName == i:
                     print("File names must be different.")
-                    errorsLog.write("ERROR in readInputFile(): Text file does not exist.")
+                    errorsLog.write("ERROR in readInputFile(): Text file does not exist.\n")
                     valid = False
                     
             if len(string) == 0:
                 if not os.path.isfile(fileName):
                     print("Text file does not exist.")
-                    errorsLog.write("ERROR in readInputFile(): Text file does not exist.")
+                    errorsLog.write("ERROR in readInputFile(): Text file does not exist.\n")
                     valid = False
             
             if not valid:
@@ -162,29 +162,33 @@ class Main:
         return fileName
     
     def __writeOutput(self, errorsLog, contents, outputFile):
-        myFile = None
+        myFile = outputFile
+        string = ""
         try:
             if not os.path.exists(outputFile):
-                myFile = open(outputFile, 'w+')
-                print(f"File created: {myFile.name}")
+                with open(outputFile, 'w+'):
+                    pass
+                print(f"\nFile created: {myFile}")
             else:
-                print("File already exists.")
-            myFile.close()
+                print("\nFile already exists. Overwriting file.")
             
             writer = open(myFile, 'w')
-            writer.write(f"First Name: {contents[0]}\n")
-            writer.write(f"Last Name: {contents[1]}\n")
-            writer.write(f"First Integer: {contents[4]}\n")
-            writer.write(f"Second Integer: {contents[5]}\n")
-            writer.write(f"Sum: {contents[2]}\n")
-            writer.write(f"Product: {contents[3]}\n")
-            writer.write(f"Input File Name: {contents[6]}\n")
-            string = self.__getInputFile(errorsLog, contents[6])
-            writer.write(f"Input File Contents:\n{string}")
+            
+            string += f"First Name: {contents[0]}\n"
+            string += f"Last Name: {contents[1]}\n"
+            string += f"First Integer: {contents[4]}\n"
+            string += f"Second Integer: {contents[5]}\n"
+            string += f"Sum: {contents[2]}\n"
+            string += f"Product: {contents[3]}\n"
+            string += f"Input File Name: {contents[6]}\n"
+            inputFileContent = self.__getInputFile(errorsLog, contents[6])
+            string += f"Input File Contents:\n{inputFileContent}"
+            writer.write(string)
             writer.close()
+            print("Output File Written")
         except:
-            print("Errot has occurred printing message")
-            errorsLog.write("ERROR in writeOutput(): File Not Found")
+            print("Error has occurred printing message")
+            errorsLog.write("ERROR in writeOutput(): File Not Found\n")
             
     def __getInputFile(self, errorsLog, input):
         fileContent = ""
@@ -192,10 +196,9 @@ class Main:
             with open(input, 'r') as file:
                 for line in file:
                     fileContent += line
-                    print(line)
         except:
             print("Cannot get input from file")
-            errorsLog.write("ERROR in getInputFile(): Cannot get input from file")
+            errorsLog.write("ERROR in getInputFile(): Cannot get input from file\n")
             
         return fileContent
                     
